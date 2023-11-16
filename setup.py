5425b2e88790e499285e04247ada8ae82980efa2
Jorge Zuluaga <zuluagajorge@gmail.com>
2023-11-16 11:04:59 -0500

    Change in setup

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         ],
-    version='0.0.2',
+    version='0.0.3',
 
     # ######################################################################
     # FILES
@@ -38,6 +38,7 @@ setuptools.setup(
     # ######################################################################
     entry_points={
         'console_scripts': ['install=fargopy.install:main'],
+        #
     },
 
     # ######################################################################
