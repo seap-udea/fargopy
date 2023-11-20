e7aeb8788379f278720757a3d443e2fcf1bb2183
Jorge Zuluaga <zuluagajorge@gmail.com>
2023-11-20 18:23:13 -0500

    New commit

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         ],
-    version='0.1.2',
+    version='0.1.3',
 
     # ######################################################################
     # FILES
