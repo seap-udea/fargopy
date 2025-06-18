66f687b827a86bfed676ee34aa3029ce046f3572
Alejo <151572058+jupitergeci@users.noreply.github.com>
2025-06-18 08:59:59 -0500

    [FIX] setup.py

--- setup.py
+++ setup.py
@@ -51,8 +51,8 @@ setuptools.setup(
     # DEPENDENCIES
     # ######################################################################
 
-    install_requires=['scipy','matplotlib','tqdm','numpy','ipython','joblib',
-                      'celluloid','psutil','gdown','pandas','plotly','ipywidgets','nbformat','cartopy'],
+    install_requires=['scipy','matplotlib','tqdm','numpy','ipython',
+                      'joblib','celluloid','psutil','gdown','pandas','plotly','ipywidgets','nbformat','cartopy'],
 
     # ######################################################################
     # OPTIONS
